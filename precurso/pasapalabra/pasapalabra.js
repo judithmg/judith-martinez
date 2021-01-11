0dd6760a870256210cf4b3815e017db120b153d5
judithmg <jmartigo33@gmail.com>
2021-01-11 19:19:25 +0100

    add pasapalabra project

---
+++ precurso/pasapalabra/pasapalabra.js
@@ -0,0 +1,649 @@
+var questions = [
+  {
+    letter: "a",
+    answer: "abducir",
+    status: 0,
+    question:
+      "CON LA A. Dicho de una supuesta criatura extraterrestre: Apoderarse de alguien",
+  },
+  {
+    letter: "a",
+    answer: "acepcion",
+    status: 0,
+    question: "CON LA A. Cada uno de los significados que tiene una palabra",
+  },
+
+  {
+    letter: "b",
+    answer: "bingo",
+    status: 0,
+    question:
+      "CON LA B. Juego que ha sacado de quicio a todos los 'Skylabers' en las sesiones de precurso",
+  },
+  {
+    letter: "b",
+    answer: "biografia",
+    status: 0,
+    question: "CON LA B. Contar la vida de una persona por escrito.",
+  },
+  {
+    letter: "c",
+    answer: "churumbel",
+    status: 0,
+    question: "CON LA C. Niño, crío, bebé",
+  },
+
+  {
+    letter: "c",
+    answer: "comic",
+    status: 0,
+    question: "CON LA C. Historia contada en viñetas con dibujos y palabras",
+  },
+
+  {
+    letter: "d",
+    answer: "diarrea",
+    status: 0,
+    question:
+      "CON LA D. Anormalidad en la función del aparato digestivo caracterizada por frecuentes evacuaciones y su consistencia líquida",
+  },
+
+  {
+    letter: "e",
+    answer: "ectoplasma",
+    status: 0,
+    question:
+      "CON LA E. Gelatinoso y se encuentra debajo de la membrana plasmática. Los cazafantasmas medían su radiación",
+  },
+
+  {
+    letter: "f",
+    answer: "facil",
+    status: 0,
+    question: "CON LA F. Que no requiere gran esfuerzo, capacidad o dificultad",
+  },
+
+  {
+    letter: "g",
+    answer: "galaxia",
+    status: 0,
+    question:
+      "CON LA G. Conjunto enorme de estrellas, polvo interestelar, gases y partículas",
+  },
+
+  {
+    letter: "h",
+    answer: "harakiri",
+    status: 0,
+    question: "CON LA H. Suicidio ritual japonés por desentrañamiento",
+  },
+
+  {
+    letter: "i",
+    answer: "iglesia",
+    status: 0,
+    question: "CON LA I. Templo cristiano",
+  },
+
+  {
+    letter: "j",
+    answer: "jabali",
+    status: 0,
+    question:
+      "CON LA J. Variedad salvaje del cerdo que sale en la película 'El Rey León', de nombre Pumba",
+  },
+
+  {
+    letter: "k",
+    answer: "kamikaze",
+    status: 0,
+    question:
+      "CON LA K. Persona que se juega la vida realizando una acción temeraria",
+  },
+
+  {
+    letter: "l",
+    answer: "licantropo",
+    status: 0,
+    question: "CON LA L. Hombre lobo",
+  },
+
+  {
+    letter: "m",
+    answer: "misantropo",
+    status: 0,
+    question:
+      "CON LA M. Persona que huye del trato con otras personas o siente gran aversión hacia ellas",
+  },
+
+  {
+    letter: "n",
+    answer: "necedad",
+    status: 0,
+    question: "CON LA N. Demostración de poca inteligencia",
+  },
+
+  {
+    letter: "ñ",
+    answer: "señal",
+    status: 0,
+    question:
+      "CONTIENE LA Ñ. Indicio que permite deducir algo de lo que no se tiene un conocimiento directo.",
+  },
+
+  {
+    letter: "o",
+    answer: "orco",
+    status: 0,
+    question:
+      "CON LA O. Humanoide fantástico de apariencia terrible y bestial, piel de color verde creada por el escritor Tolkien",
+  },
+
+  {
+    letter: "p",
+    answer: "protoss",
+    status: 0,
+    question:
+      "CON LA P. Raza ancestral tecnológicamente avanzada que se caracteriza por sus grandes poderes psíonicos del videojuego StarCraft",
+  },
+
+  {
+    letter: "q",
+    answer: "queso",
+    status: 0,
+    question:
+      "CON LA Q. Producto obtenido por la maduración de la cuajada de la leche",
+  },
+
+  {
+    letter: "r",
+    answer: "raton",
+    status: 0,
+    question: "CON LA R. Roedor",
+  },
+
+  {
+    letter: "s",
+    answer: "stackoverflow",
+    status: 0,
+    question: "CON LA S. Comunidad salvadora de todo desarrollador informático",
+  },
+
+  {
+    letter: "t",
+    answer: "terminator",
+    status: 0,
+    question:
+      "CON LA T. Película del director James Cameron que consolidó a Arnold Schwarzenegger como actor en 1984",
+  },
+
+  {
+    letter: "u",
+    answer: "unamuno",
+    status: 0,
+    question:
+      "CON LA U. Escritor y filósofo español de la generación del 98 autor del libro 'Niebla' en 1914",
+  },
+
+  {
+    letter: "v",
+    answer: "vikingos",
+    status: 0,
+    question:
+      "CON LA V. Nombre dado a los miembros de los pueblos nórdicos originarios de Escandinavia, famosos por sus incursiones y pillajes en Europa",
+  },
+
+  {
+    letter: "w",
+    answer: "sandwich",
+    status: 0,
+    question:
+      "CONTIENE LA W. Emparedado hecho con dos rebanadas de pan entre las cuales se coloca jamón y queso",
+  },
+
+  {
+    letter: "x",
+    answer: "botox",
+    status: 0,
+    question: "CONTIENE LA X. Toxina bacteriana utilizada en cirujía estética",
+  },
+
+  {
+    letter: "y",
+    answer: "peyote",
+    status: 0,
+    question:
+      "CONTIENE LA Y. Pequeño cáctus conocido por sus alcaloides psicoactivos utilizado de forma ritual y medicinal  por indígenas americanos",
+  },
+
+  {
+    letter: "z",
+    answer: "zen",
+    status: 0,
+    question:
+      "CON LA Z. Escuela de budismo que busca la experiencia de la sabiduría más allá del discurso racional",
+  },
+
+  {
+    letter: "d",
+    answer: "diccionario",
+    status: 0,
+    question:
+      "CON LA D. Libro en el que aparece el significado de las palabras por orden alfabético",
+  },
+
+  {
+    letter: "e",
+    answer: "esdrujula",
+    status: 0,
+    question: "CON LA E. Palabra cuya sílaba tónica es la antepenúltima",
+  },
+
+  {
+    letter: "f",
+    answer: "frase",
+    status: 0,
+    question: "CON LA F. Tipo de enunciado que no tiene verbo",
+  },
+
+  {
+    letter: "g",
+    answer: "grave",
+    status: 0,
+    question: "CON LA G. Antónimo de leve",
+  },
+
+  {
+    letter: "h",
+    answer: "homofonas",
+    status: 0,
+    question:
+      "CON LA H. Palabras que se pronunciamos igual, pero tienen significados diferentes",
+  },
+
+  {
+    letter: "",
+    answer: "",
+    status: 0,
+    question: "CON LA . ",
+  },
+
+  {
+    letter: "i",
+    answer: "infinitivo",
+    status: 0,
+    question: "CON LA I. Forma no personal del verbo",
+  },
+
+  {
+    letter: "j",
+    answer: "jimenez",
+    status: 0,
+    question: "CON LA J. Apellido del autor de 'Platero y yo'",
+  },
+
+  {
+    letter: "k",
+    answer: "frankenstein",
+    status: 0,
+    question:
+      "CONTIENE LA K. Título de una novela y nombre de su protagonista, un sabio que da vida a un monstruo componiendo miembros de cadáveres",
+  },
+
+  {
+    letter: "l",
+    answer: "lope de vega",
+    status: 0,
+    question: "CON LA L. Gran escritor español perteneciente al Siglo de Oro",
+  },
+
+  {
+    letter: "m",
+    answer: "monosilaba",
+    status: 0,
+    question: "CON LA M. Palabra que contiene una sola sílaba",
+  },
+
+  {
+    letter: "n",
+    answer: "numeral",
+    status: 0,
+    question: "CON LA N. Determinante que indica cantidad u orden",
+  },
+
+  {
+    letter: "ñ",
+    answer: "añorar",
+    status: 0,
+    question: "CONTIENE LA Ñ. Sinónimo de extrañar, echar de menos",
+  },
+
+  {
+    letter: "p",
+    answer: "polisemicas",
+    status: 0,
+    question: "CON LA P. Palabras que tienen más de tres sílabas",
+  },
+
+  {
+    letter: "o",
+    answer: "ortografaa",
+    status: 0,
+    question: "CON LA O. Conjunto de normas que regulan la escritura",
+  },
+
+  {
+    letter: "q",
+    answer: "enclenque",
+    status: 0,
+    question:
+      "CONTIENE LA Q. Dícese de la persona débil, enfermiza, muy flaca ",
+  },
+  {
+    letter: "r",
+    answer: "receptor",
+    status: 0,
+    question: "CON LA R. El que recibe la información",
+  },
+
+  {
+    letter: "s",
+    answer: "sobreesdrujula",
+    status: 0,
+    question:
+      "CON LA S. Palabra cuya sílaba tónica es la anterior a la antepenúltima",
+  },
+
+  {
+    letter: "t",
+    answer: "tragedia",
+    status: 0,
+    question: "CON LA T. Obra de teatro que tiene un desenlace desgraciado",
+  },
+
+  {
+    letter: "u",
+    answer: "undecimo",
+    status: 0,
+    question:
+      "CON LA U. Adjetivo que sigue inmediatamente en orden al o a lo décimo",
+  },
+
+  {
+    letter: "v",
+    answer: "verso",
+    status: 0,
+    question:
+      "CON LA V. Cada uno de los renglones cortos que forman una poesía",
+  },
+
+  {
+    letter: "w",
+    answer: "what",
+    status: 0,
+    question: "CON LA W. 'Qué' en inglés",
+  },
+
+  {
+    letter: "x",
+    answer: "extra",
+    status: 0,
+    question: "CONTIENE LA X. Prefijo que significa “fuera de",
+  },
+
+  {
+    letter: "y",
+    answer: "onomatopeya",
+    status: 0,
+    question: "CONTIENE LA Y. Vocablo que imita o recrea un sonido",
+  },
+
+  {
+    letter: "z",
+    answer: "zumbido",
+    status: 0,
+    question: "CON LA Z. Ruido que hacen algunos insectos como las abejas",
+  },
+];
+
+//hacer que me genere un número random. ese objeto, compararlo con la letra que me toca. si coincide, leer esa pregunta. si no, saltar a la siguiente.
+
+let abcd = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "ñ",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let min = 0;
+let max = questions.length - 1;
+let random;
+let question;
+let thisletter = 0;
+let points = 0;
+
+const RIGHT = "¡Respuesta correcta!\n";
+const WRONG = "Respuesta incorrecta :(.\n";
+const SAVE = "Pasapalabra.\n";
+const LETTER = abcd[thisletter];
+const PASAPALABRA = [];
+const PASAPALABRApos = 0; //posición que recorre el array cuando se cumple la primera vuelta
+
+const WRONGcons = "RESPUESTA INCORRECTA. La respuesta correcta era: ";
+const PASAPALABRAcons = "La palabra se guardará para el final.";
+const RIGHTcons = "¡RESPUESTA CORRECTA! Un punto suma a tu marcador.";
+
+let user = username();
+const player1 = {
+  user: "Josuke",
+  score: 50,
+};
+const player2 = {
+  user: "Jotaro",
+  score: 60,
+};
+const player3 = {
+  user: "Joseph",
+  score: 66,
+};
+const player4 = {
+  user: "Jonathan",
+  score: 49,
+};
+let thisplayer;
+let ranks;
+
+function Player(user, score) {
+  this.user = user;
+  this.score = score;
+}
+
+function username() {
+  let user = "";
+
+  do {
+    user = prompt(
+      "¡Bienvenido al PASAPALABRA VERSIÓN SKYLAB! \nPor favor, introduce el nombre de usuario"
+    );
+    if (user === null || user === "") {
+      alert("Por favor, introduce un nombre.");
+    }
+  } while (user === "" || user === null || !user.match(/^[A-Za-z]+$/));
+
+  return user;
+}
+
+function maingame() {
+  console.warn(
+    `Deberás adivinar la palabra que se esconde tras las 25 letras del abecedario después de leer la descripción. Cada palabra correcta sumará un punto a tu marcador.\n\nEscribe 'PASAPALABRA' en caso de querer guardar la palabra para el final\n\nEscribe 'END' en caso de querer acabar el juego\n\nEn este PASAPALABRA, pedimos que NO SE USEN ACENTOS.\n\nSOLO SE ACEPTAN LETRAS.\n\nAl final del juego se mostrará el ranking.\n\n¡El juego dará inicio a continuación!`
+  );
+  alert(
+    `${user}, te recordamos las normas del PASAPALABRA en consola. Una vez las hayas leído, pulsa 'ACEPTAR'.`
+  );
+  firstquestion();
+
+  pasapalabraover();
+  console.table(ranks);
+
+  return;
+}
+
+function firstquestion() {
+  do {
+    random = getRandomNumber(min, max);
+  } while (questions[random]["letter"] !== abcd[thisletter]);
+
+  do {
+    question = prompt(questions[random]["question"]);
+    if (question === null) {
+      alert('Por favor, en caso de querer salir, escribe "END".');
+    } else if (!question.match(/^[A-Za-z]+$/)) {
+      alert(
+        "Solo se aceptan letras. Te recomendamos que vuelvas a leer las normas."
+      );
+    }
+  } while (question === null || !question.match(/^[A-Za-z]+$/));
+
+  question = question.toLowerCase();
+
+  if (question === "end") {
+    endgame();
+  } else if (question === questions[random]["answer"]) {
+    points++;
+    abcd.splice(thisletter, 1);
+
+    console.log(
+      `Con la ${questions[random]["letter"].toUpperCase()}, ${RIGHTcons}`
+    );
+    nextanswer(RIGHT);
+    return;
+  } else if (question === "pasapalabra") {
+    abcd.push(abcd[thisletter]);
+    abcd.splice(thisletter, 1);
+
+    console.log(`${PASAPALABRAcons}`);
+    nextanswer(SAVE);
+    return;
+  } else {
+    abcd.splice(thisletter, 1);
+
+    console.warn(
+      `Con la ${questions[random][
+        "letter"
+      ].toUpperCase()}, ${WRONGcons}${questions[random][
+        "answer"
+      ].toUpperCase()}`
+    );
+    nextanswer(WRONG);
+    return;
+  }
+
+  return;
+}
+
+function nextanswer(nextprompt) {
+  if (abcd.length !== 0) {
+    do {
+      random = getRandomNumber(min, max);
+    } while (questions[random]["letter"] !== abcd[thisletter]);
+
+    do {
+      question = prompt(`${nextprompt} ${questions[random]["question"]}`);
+
+      if (question === null) {
+        alert('Por favor, en caso de querer salir, escribe "END".');
+      } else if (!question.match(/^[A-Za-z]+$/)) {
+        alert(
+          "Solo se aceptan letras. Te recomendamos que vuelvas a leer las normas."
+        );
+      }
+    } while (question === null || !question.match(/^[A-Za-z]+$/));
+
+    question = question.toLowerCase();
+    if (question === "end") {
+      endgame();
+    } else if (question === questions[random]["answer"]) {
+      points++;
+      abcd.splice(thisletter, 1);
+
+      console.log(
+        `Con la ${questions[random]["letter"].toUpperCase()}, ${RIGHTcons}`
+      );
+      nextanswer(RIGHT);
+      return;
+    } else if (question === "pasapalabra") {
+      abcd.push(abcd[thisletter]);
+      abcd.splice(thisletter, 1);
+
+      console.log(`${PASAPALABRAcons}`);
+      nextanswer(SAVE);
+      return;
+    } else {
+      abcd.splice(thisletter, 1);
+
+      console.warn(
+        `Con la ${questions[random][
+          "letter"
+        ].toUpperCase()}, ${WRONGcons}${questions[random][
+          "answer"
+        ].toUpperCase()}`
+      );
+      nextanswer(WRONG);
+      return;
+    }
+
+    return;
+  }
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function endgame() {
+  alert(
+    "El juego dará final a continuación. Se añadirá tu puntuación al ranking."
+  );
+  return;
+}
+
+function pasapalabraover() {
+  thisplayer = new Player(user, points);
+  ranks = ranking();
+  alert(
+    `¡El juego ha finalizado! Tu puntuación final es de ${points}. Tu posición en el ranking se muestra en consola. ¡Felicidades!`
+  );
+  return;
+}
+
+function ranking() {
+  let ranked = [];
+  ranked.push(thisplayer);
+  ranked.push(player1, player2, player3, player4);
+  ranked.sort((a, b) => b.score - a.score);
+  return ranked;
+}
+
+maingame();
