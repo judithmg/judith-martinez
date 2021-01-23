fd8212823a2ff1cf67b391eb1715307f4fcae79e
judithmg <jmartigo33@gmail.com>
2021-01-23 16:13:38 +0100

    app almost done

--- game-of-life/app.js
+++ game-of-life/app.js
@@ -1,40 +1,127 @@
 console.log("hello world");
 
-let CHART = [];
-let ROWS = "";
-let COLUMNS = "";
-
 /* SELECTORS */
 let tableElement = document.querySelector(".drawn-chart");
 
 function createChart(rows, columns) {
   let chart = [];
   for (let i = 0; i < rows; i++) {
-    let current = [];
+    let currentRow = [];
     for (let j = 0; j < columns; j++) {
-      current.push(0);
+      currentRow.push(0);
     }
-    chart.push(current);
+    chart.push(currentRow);
   }
   return chart;
 }
 
-function checkNeighbours() {
-  let currentCell;
-
+/* function checkNeighbours(rows, columns, chart) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
-      currentCell = chart[i][j];
+      let previousRow = chart[i - 1] || false;
+      let currentRow = chart[i];
+      let nextRow = chart[i + 1] || false;
+
+      let aliveAroundCurrent =
+        +!!previousRow[j - 1] +
+        +!!previousRow[j] +
+        +!!previousRow[j + 1] +
+        +!!currentRow[j + 1] +
+        +!!currentRow[j - 1] +
+        +!!nextRow[j - 1] +
+        +!!nextRow[j] +
+        +!!nextRow[j + 1];
+
+      console.log(aliveAroundCurrent);
+      if (aliveAroundCurrent > 1) {
+        console.log("this is i " + i + "and j is " + j);
+      }
     }
-    chart.push(current);
+  }
+} */
+
+//WITH THIS FUNCTION, FOR A GIVEN CELL, I WILL CHECK ITS SURROUNDING NEIGHBOURS
+
+function createNextGen(rows, columns, chart) {
+  let fullGen = [...chart];
+  for (let i = 0; i < rows; i++) {
+    let thisRow = [...fullGen[i]];
+    for (let j = 0; j < columns; j++) {
+      //checkNeighbours(i, j, chart);
+      let nextGenCell = updateCurrentCell(i, j, chart);
+
+      thisRow[j] = nextGenCell;
+    }
+    fullGen[i] = [...thisRow];
+  }
+  return fullGen;
+}
+
+function checkNeighbours(i, j, chart) {
+  let previousRow = chart[i - 1] || false;
+  let currentRow = chart[i];
+  let nextRow = chart[i + 1] || false;
+
+  let aliveAroundCurrent =
+    +!!previousRow[j - 1] +
+    +!!previousRow[j] +
+    +!!previousRow[j + 1] +
+    +!!currentRow[j + 1] +
+    +!!currentRow[j - 1] +
+    +!!nextRow[j - 1] +
+    +!!nextRow[j] +
+    +!!nextRow[j + 1];
+
+  return aliveAroundCurrent;
+}
+
+// Any live cell with two or three live neighbours survives.
+// Any dead cell with three live neighbours becomes a live cell.
+// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+/* function updateThisGen(i, j, chart) {
+  let currentCell = chart[i][j];
+  let nextGenCell;
+  let currentRow = [...chart[i]];
+  //This variable stores the state of the cell i'm checking but for ITS NEXT GEN because the current gen must stay the same state!!
+  let surroundingCellsAlive = checkNeighbours(i, j, chart);
+  if (surroundingCellsAlive > 1 && currentCell > 1) {
+    nextGenCell = 1;
+  } else if (surroundingCellsAlive > 2 && currentCell === 0) {
+    nextGenCell = 1;
+  } else {
+    nextGenCell = 0;
   }
 
-  chart.map((el) => {
-    el.map((num) => {
-      console.log(num);
-    });
-  });
-  //esto me itera por todo
+  updatedGen = [...chart];
+  currentRow[i] = nextGenCell;
+  updatedGen[i] = [...currentRow];
+
+  return updatedGen;
+} */
+
+function updateCurrentCell(i, j, chart) {
+  let currentCell = chart[i][j];
+  let nextGenCell;
+  //This variable stores the state of the cell i'm checking but for ITS NEXT GEN because the current gen must stay the same state!!
+  let surroundingCellsAlive = checkNeighbours(i, j, chart);
+
+  if (currentCell === 1 && surroundingCellsAlive < 2) {
+    nextGenCell = 0;
+  } else if (
+    currentCell === 1 &&
+    (surroundingCellsAlive === 2 || surroundingCellsAlive === 3)
+  ) {
+    nextGenCell = 1;
+  } else if (currentCell === 1 && surroundingCellsAlive > 3) {
+    nextGenCell = 0;
+  } else if (currentCell === 0 && surroundingCellsAlive === 3) {
+    nextGenCell = 1;
+  } else {
+    nextGenCell = currentCell;
+  }
+
+  return nextGenCell;
 }
 
 function drawChart(rows, columns) {
@@ -47,7 +134,7 @@ function drawChart(rows, columns) {
     for (let j = 0; j < columns; j++) {
       let tempCell = document.createElement("div");
       tempCell.classList.add("cell");
-      tempCell.classList.add(`'row-${j}-${i}`);
+      tempCell.classList.add(`'cell-${j}-${i}`);
 
       let tempRow = document.querySelector(`.row-${i}`);
 
@@ -56,7 +143,10 @@ function drawChart(rows, columns) {
   }
 }
 
-ROWS = 10;
-COLUMNS = ROWS;
-CHART = createChart(ROWS, COLUMNS);
+const ROWS = 5;
+const COLUMNS = ROWS;
+const CHART = createChart(ROWS, COLUMNS);
+CHART[2] = [0, 1, 1, 1, 0];
+CHART[3][1] = 1;
+
 drawChart(ROWS, COLUMNS);
